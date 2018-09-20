819f3c6d1d754a48ceefbd3d9327079ef9a32005
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-20 13:55:36 +0200

    java exercise 7

---
+++ week-01/day-4-java/src/Exercise07.java
@@ -0,0 +1,13 @@
+public class Exercise07 {
+    public static void main(String[] args) {
+        int favoriteNumber = 8;
+        // Store your favorite number in a variable (as a number)
+        // And print it like this: "My favorite number is: 8"
+
+        int favorite = 68;
+        System.out.println("My favorite number is: " + favorite);
+
+    }
+
+
+}
