a8f397ea7fb2c67a1d472132bc2a38bc1f6c28f9
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-20 17:34:27 +0200

    eXercise 20

--- week-01/day-4-java/src/Exercise20.java
+++ week-01/day-4-java/src/Exercise20.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Exercise20 {
+    // Write a program that asks for two numbers and prints the bigger one
+    public static void main(String[] args){
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Add 1st number");
+        int numberOne = myScanner.nextInt();
+        System.out.println("Add 2nd number");
+        int numberTwo = myScanner.nextInt();
+
+        if(numberOne>numberTwo){
+            System.out.println(numberOne);
+        }
+
+        if (numberOne<numberTwo){
+            System.out.println(numberTwo);
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
+
