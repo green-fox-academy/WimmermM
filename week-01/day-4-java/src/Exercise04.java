5e48eac9743450ed4b48c480defd801c07a4e3f0
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-20 11:36:09 +0200

    java exercise 4

---
+++ week-01/day-4-java/src/Exercise04.java
@@ -0,0 +1,25 @@
+public class Exercise04 {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+
+        String myName = "Marek Wimmer";
+        int myAge = 29;
+        double myHeight = 1.79;
+
+        System.out.println("My name is " + myName);
+        System.out.println("I am " + myAge + " years old");
+        System.out.println("I am " + myHeight +"cm tall");
+
+
+
+    }
+}
