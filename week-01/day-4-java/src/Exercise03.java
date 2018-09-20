99216e267535d01e438893bfd96d51d26209d2d5
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-20 11:26:48 +0200

    java exercise 3

---
+++ week-01/day-4-java/src/Exercise03.java
@@ -0,0 +1,18 @@
+public class Exercise03 {
+        public static void main(String[] args) {
+            // Greet 3 of your classmates with this program, in three separate lines
+            // like:
+            //
+            // Hello, Esther!
+            // Hello, Mary!
+            // Hello, Joe!
+
+            System.out.println("HEllo, Honza");
+            System.out.println("HEllo, Ivanka");
+            System.out.println("HEllo, Tomek");
+
+
+        }
+
+    }
+
