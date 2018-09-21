00d5bbabf915a8256d2624536e37ee64f3e54bd5
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-21 17:44:21 +0200

    pyramid...

--- week-01/day-4-java/src/Exercise29.java
+++ week-01/day-4-java/src/Exercise29.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Exercise29 {
+
+    // Write a program that reads a number from the standard input, then draws a
+// pyramid like this:
+//
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as the number was
+
+
+    public static void main(String[] args) {
+
+        for (int a = 0; a<5;a++){
+            for (int b = 0;b<5-a;b++){
+                System.out.print(" ");
+            }
+            for (int c =0;c<a;c++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
