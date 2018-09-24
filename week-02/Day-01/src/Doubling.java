255c7fb8bc27ff7b0f8c30c4f2d12bfb0ea8ec8a
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-24 12:20:12 +0200

    exercise Doubling a Greeter

---
+++ week-02/Day-01/src/Doubling.java
@@ -0,0 +1,22 @@
+public class Doubling {
+
+    public static int baseNum = 123;
+
+    public static void main(String[] args) {
+
+        // - Create an integer variable named `baseNum` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+        // - Print the result of `doubling(baseNum)`
+
+        System.out.println("Result " + doubling());
+
+    }
+
+    public  static int doubling (){
+        return (baseNum * 2);
+
+
+
+    }
+}
+
---
+++ week-02/Day-01/src/Greeter.java
@@ -0,0 +1,33 @@
+public class Greeter {
+    //  From now on, create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Greenfox`
+// - Greet `al`
+
+    static String al = "Greenfox";
+
+    public static void main(String[] args) {
+
+        System.out.println(greet());
+
+    }
+
+
+    public static String greet (){
+
+        return ("Greetings dear ," + al);
+
+    }
+}
+
+
+
+
+
+
+
+
+
