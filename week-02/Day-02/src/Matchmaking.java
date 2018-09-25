cee10351b2c2bd446ff3933b45e68a234ab8af80
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-25 17:50:11 +0200

    exercises for day 2 last ones

---
+++ week-02/Day-02/src/Matchmaking.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+// Write a method that joins the two lists by matching one girl with one boy into a new list
+// Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+
+
+    public class Matchmaking{
+        public static void main(String... args){
+            ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane"));
+            ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
+
+            System.out.println(makingMatches(girls, boys));
+        }
+
+
+        public static ArrayList makingMatches(ArrayList boysGrils){
+
+            
+
+
+
+
+            return ;
+        }
+
+
+
+}
--- week-02/Day-02/src/SolarSystem.java
+++ week-02/Day-02/src/SolarSystem.java
@@ -1,2 +1,38 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Saturn is missing from the planetList
+// Insert it into the correct position
+// Create a method called putSaturn() which has list parameter and returns the correct list.
+// Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+
+
+
 public class SolarSystem {
-}
+
+    public static void main(String... args){
+        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+
+        System.out.println(putSaturn(planetList));
+    }
+
+
+    public static ArrayList putSaturn (ArrayList planets){
+
+        planets.add(5,"Saturn");
+
+
+        return planets;
+    }
+
+
+
+
+    }
+
+
+
+
+
