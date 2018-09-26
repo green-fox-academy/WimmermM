c0e75e3e314a42b24d93e84619a6a1ea7997bee2
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-26 15:01:38 +0200

    adding Square position

--- week-02/Day-03/src/SquarePosition.java
+++ week-02/Day-03/src/SquarePosition.java
@@ -3,44 +3,44 @@ import javax.swing.*;
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
-import static javax.swing.JFrame.setDefaultLookAndFeelDecorated;
 
-public class Rect {
+public class SquarePosition {
 
     public static void mainDraw(Graphics graphics){
-        // draw four different size and color rectangles.
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
         // avoid code duplication.
 
-        duplication(10,6, 8,13,graphics);
+        square(10,20,graphics);
 
     }
 
 
+    public static void square(int a, int b, Graphics graphics){
 
-    public static void duplication(int a,int b, int c, int d,Graphics graphics){
+        for (int i = 0; i < 3; i++) {
 
-        for (int i = 0; i < 4; i++) {
+            graphics.setColor(Color.BLACK);
+            graphics.fillRect(a,b,50,50);
 
-            int r = 100 + (int)(Math.random()*100);
-            int g = 100 + (int)(Math.random()*100);
-            int w = 100 + (int)(Math.random()*100);
+            a=a+50;
+            b=b+50;
 
-
-            Color myColor = new Color(r,g,w);
-            graphics.setColor(myColor);
-            graphics.fillRect(a,b,c,d);
-
-            a=a+=75;
-            b=b+=12;
-            c=c+=30;
-            d=d+=20;
         }
 
+
     }
+
+
+
+
 
     //    Don't touch the code below
     static int WIDTH = 320;
-    static int HEIGHT = 320;
+    static int HEIGHT = 343;
+
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
         jFrame.setSize(new Dimension(WIDTH, HEIGHT));
