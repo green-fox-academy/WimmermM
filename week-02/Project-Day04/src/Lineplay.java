df6c293a6ac588ba67cad89ecc11c1309ad4354a
Marek Wimmer <marek.wimmer@gmail.com>
2018-09-27 12:02:06 +0200

    Lineplay Project

---
+++ week-02/Project-Day04/src/Lineplay.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Lineplay {
+    public static void mainDraw(Graphics graphics){
+        // draw a red horizontal line to the canvas' middle.
+        // draw a green vertical line to the canvas' middle.
+        
+
+
+        for (int i = 0; i < 320; i+=20) {
+
+            graphics.setColor(Color.GREEN);
+            graphics.drawLine(WIDTH -i,HEIGHT,0,HEIGHT-i);
+            graphics.setColor(Color.MAGENTA);
+            graphics.drawLine(0+i,0,WIDTH,i);
+
+
+
+
+        }
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
