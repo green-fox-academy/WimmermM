50c3a2b9088fa278dacb3f660ce51b7ab6ce1119
Marek Wimmer <marek.wimmer@gmail.com>
2018-10-05 15:30:38 +0200

    carpet..

--- week-03/Day-05/src/SierpinskyCarpet.java
+++ week-03/Day-05/src/SierpinskyCarpet.java
@@ -2,36 +2,35 @@ import javax.swing.*;
 
 import java.awt.*;
 
+import static java.awt.Window.getWindows;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class SierpinskyCarpet {
     public static void mainDraw(Graphics graphics){
-        // draw a red horizontal line to the canvas' middle.
-        // draw a green vertical line to the canvas' middle.
 
-//        carpet(WIDTH/2 -25,HEIGHT/2 -25,graphics);
-    }
-
-    public static void carpet (int x,int y, Graphics graphics){
-        graphics.fillRect(x,y,50,50);
-
+        carpet(0,0,WIDTH,graphics);
 
     }
 
-    public static int checkForSpace(int x, int y){
-        while (x > 0 || y > 0) // when either of these reaches zero the pixel is determined to be on the edge
-        // at that square level and must be filled
-        {
-            if (x % 3 == 1 && y % 3 == 1) //checks if the pixel is in the center for the current square level
-                return 0;
-            x /= 3; //x and y are decremented to check the next larger square level
-            y /= 3;
+    public static void carpet (int x,int y,int size, Graphics graphics){
+
+        if (size > 0){
+            graphics.fillRect(size/3 + x,size/3 + y,size/3, size/3);
+
+            carpet(x,y,size/3,graphics);
+            carpet(size * 2/3 + x,size * 2/3 + y,size/3,graphics);
+            carpet(size*2/3 +x,y,size/3,graphics);
+            carpet(x,size * 2/3 +y,size/3,graphics);
+            carpet(x,size * 1/3 +y,size/3,graphics);
+            carpet(size * 1/3 + x, y, size/3,graphics);
+            carpet(size * 1/3 + x,size * 2/3 + y,size/3,graphics);
+            carpet(size* 2/3 + x,size*1/3 + y,size/3,graphics);
+
+
         }
-        return 1; // if all possible square levels are checked and the pixel is not determined
-        // to be open it must be filled
-
 
     }
+
 
 
 
