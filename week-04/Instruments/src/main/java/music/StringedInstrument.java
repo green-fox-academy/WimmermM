f29fe2c4a7304ccb8d2d77024c8f5d27152129fd
Marek Wimmer <marek.wimmer@gmail.com>
2018-10-09 18:03:56 +0200

    music work in progress

--- week-04/Instruments/src/main/java/music/StringedInstrument.java
+++ week-04/Instruments/src/main/java/music/StringedInstrument.java
@@ -12,9 +12,51 @@ public abstract class StringedInstrument extends Instrument {
     }
 
 
+
+
     public void sound(){
-        this.play();
+
     }
 
 
+}
+
+
+class ElectricGuitar extends StringedInstrument{
+
+
+    public ElectricGuitar(int numberOfStrings) {
+        super(6);
+    }
+
+
+    @Override
+    public void play() {
+        System.out.println("Twang");
+    }
+}
+
+class BassGuitar extends StringedInstrument{
+
+
+    public BassGuitar(int numberOfStrings) {
+        super(4);
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Duum-duum-duum");
+    }
+}
+
+class Violin extends StringedInstrument{
+
+    public Violin(int numberOfStrings) {
+        super(4);
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Screech");
+    }
 }
