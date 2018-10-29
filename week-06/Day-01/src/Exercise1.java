492aa56176690ebb5d9a5ff48a17e06dd019438c
Marek Wimmer <marek.wimmer@gmail.com>
2018-10-29 13:20:10 +0100

    refactoring name of main to exercise1

--- week-06/Day-01/src/Exercise1.java
+++ week-06/Day-01/src/Exercise1.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Main {
+public class Exercise1 {
 
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
