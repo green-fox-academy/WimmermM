042b6070a4fa4b177b84017d720326c3f279a6ee
Marek Wimmer <marek.wimmer@gmail.com>
2018-10-29 16:10:29 +0100

    finished exercise 8

---
+++ week-06/Day-01/src/Exercise8.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Exercise8 {
+
+    //Write a Stream Expression to find the frequency of numbers in the following array:
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+        Map<Integer,Long> myMap = numbers.stream().
+                collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        System.out.println(myMap);
+
+
+    }
+
+
+}
