c3d587733a271fc2f7a6a9a258c7189cabfc3aae
Marek Wimmer <marek.wimmer@gmail.com>
2018-10-29 14:24:57 +0100

    finished exercise 5

--- week-06/Day-01/src/Exercise5.java
+++ week-06/Day-01/src/Exercise5.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//Write a Stream Expression to find the uppercase characters in a string!
 public class Exercise5 {
 
     public static void main(String[] args) {
 
-        
+//        ArrayList<String> words = new ArrayList<>(Arrays.asList("Ano", "nE", "PeS", "KocKa", "AuTo", "JidLo", "stROM"));
+
+        String gusta = "GuSaV";
+
+        gusta.chars().filter(w -> (Character.isUpperCase(w))).forEach(w -> System.out.println((char)w));
+
     }
 }
